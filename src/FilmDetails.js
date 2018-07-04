002e10d35033caa33d5e75161179a52efddbcab7
William Connelly <williamconnelly.dev@gmail.com>
2018-07-03 23:27:50 -0700

    Step 1 & 2

---
+++ src/FilmDetails.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FilmDetails = props => (
+    <div className="film-details">
+        <h1 className="section-title">DETAILS</h1>
+    </div>
+)
+
+export default FilmDetails;
---
+++ src/FilmListing.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FilmListing = props => (
+    <div className="film-list">
+        <h1 className="section-title">FILMS</h1>
+    </div>
+)
+
+export default FilmListing;
