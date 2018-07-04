6c160c4e2e572fe9b3b3771486f69b9bfd7a8f5b
William Connelly <williamconnelly.dev@gmail.com>
2018-07-03 23:43:34 -0700

    Steps 3 & 4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import FilmListing from "./FilmListing";
 import FilmDetails from "./FilmDetails";
+import TMDB from "./TMDB";
+
+const films = TMDB.films;
+console.log(films);
 
 class App extends Component {
     render() {
         return (
             <div className="film-library">
-                <FilmListing />
-             <FilmDetails />
+                <FilmListing films={films[0].title}/>
+             <FilmDetails/>
             </div>
         );
     }
--- src/FilmListing.js
+++ src/FilmListing.js
@@ -3,6 +3,7 @@ import React from "react";
 const FilmListing = props => (
     <div className="film-list">
         <h1 className="section-title">FILMS</h1>
+        <h1>{props.films}</h1>
     </div>
 )
 
