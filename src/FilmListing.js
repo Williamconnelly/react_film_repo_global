4c65ee09a4a4a1f2952b5dc6e044734a2f30a3f3
William Connelly <williamconnelly.dev@gmail.com>
2018-07-05 23:00:23 -0700

    Completed Task 5

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -6,11 +6,14 @@ class FilmListing extends Component {
         super(props)
         this.handleFilterClick = this.handleFilterClick.bind(this)
         this.state = {
-            
+            filter: "all"
         }
     }
     handleFilterClick(filter) {
         console.log(`Setting Filter to ${filter}`);
+        this.setState({
+            filter: filter
+        })
     }
 
     render() {
@@ -20,15 +23,17 @@ class FilmListing extends Component {
             )
         })
 
+        const checkFilter = this.state.filter === 'all' ? 'is-active' : ""
+
         return (
             <div className="film-list">
                 <h1 className="section-title">FILMS</h1>
                 <div className="film-list-filters">
-                    <div className="film-list-filter" onClick= {() => {this.handleFilterClick("all")}}>
+                    <div className={"film-list-filter " + checkFilter} onClick= {() => {this.handleFilterClick("all")}}>
                         ALL
                         <span className="section-count">{this.props.films.length}</span>
                     </div>
-                    <div className="film-list-filter" onClick= {() => {this.handleFilterClick("faves")}}>
+                    <div className={"film-list-filter " + checkFilter} onClick= {() => {this.handleFilterClick("faves")}}>
                         FAVES
                         <span className="section-count">0</span>
                     </div>
